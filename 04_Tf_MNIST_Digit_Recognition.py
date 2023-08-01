7009daf3e52f9d82a6537719a38d7a9a5104156b
Sambit K Satapathy <123438716+SambitSatapathy9@users.noreply.github.com>
2023-08-01 17:38:54 +0530

    Create 04_Tf_MNIST_Digit_Recognition.py

---
+++ 04_Tf_MNIST_Digit_Recognition.py
@@ -0,0 +1,23 @@
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras.Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
+
+#You can load the model from the MNIST website. The codes for loading the data is not shown here. 
+#THe following code is to show the model with an input layer, 2 hidden layers with 25 and 15 neurons respectively and an output layer with 10 outputs, i.e., the digits 0 to 9.
+
+model = Sequential([
+    Dense(25, activation = 'relu'),
+    Dense(15, activation = 'relu'), 
+    Dense(10, activation = 'softmax')
+])
+
+#Compile the model
+model.compile(loss = SparseCategoricalCrossentropy())
+
+#Fit the model
+model.fit(X,y, epochs = 100)
+
+#Prediction
+pred = model.predict(X)
