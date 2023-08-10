d9e51b35d68a961f732111fcbc3758308b79e49c
Sambit K Satapathy <123438716+SambitSatapathy9@users.noreply.github.com>
2023-08-10 23:58:22 +0530

    Update 10_Polynomial_features.py

--- 10_Polynomial_features.py
+++ 10_Polynomial_features.py
@@ -269,12 +269,95 @@ Another way to look at it is to observe the points at degree=4. You'll notice th
 is lower with 3 features, the *gap between the training error and cross validation error* is a lot wider than when 
 you only use 2 features. This should also warn you that the model is overfitting.
 """
+def train_plot_diff_datasets(model, files, max_degree=10, baseline=None):
+    
+    for file in files:
+        
+        x_train, y_train, x_cv, y_cv, x_test, y_test = prepare_dataset(file['filename'])
 
+        train_mses = []
+        cv_mses = []
+        models = []
+        scalers = []
+        degrees = range(1,max_degree+1)
 
+        # Loop over 10 times. Each adding one more degree of polynomial higher than the last.
+        for degree in degrees:
 
+            # Add polynomial features to the training set
+            poly = PolynomialFeatures(degree, include_bias=False)
+            X_train_mapped = poly.fit_transform(x_train)
 
+            # Scale the training set
+            scaler_poly = StandardScaler()
+            X_train_mapped_scaled = scaler_poly.fit_transform(X_train_mapped)
+            scalers.append(scaler_poly)
 
+            # Create and train the model
+            model.fit(X_train_mapped_scaled, y_train )
+            models.append(model)
 
+            # Compute the training MSE
+            yhat = model.predict(X_train_mapped_scaled)
+            train_mse = mean_squared_error(y_train, yhat) / 2
+            train_mses.append(train_mse)
+
+            # Add polynomial features and scale the cross-validation set
+            poly = PolynomialFeatures(degree, include_bias=False)
+            X_cv_mapped = poly.fit_transform(x_cv)
+            X_cv_mapped_scaled = scaler_poly.transform(X_cv_mapped)
+
+            # Compute the cross-validation MSE
+            yhat = model.predict(X_cv_mapped_scaled)
+            cv_mse = mean_squared_error(y_cv, yhat) / 2
+            cv_mses.append(cv_mse)
+
+        # Plot the results
+        plt.plot(degrees, train_mses, marker='o', c='r', linestyle=file['linestyle'], label=f"{file['label']} training MSEs", linewidth=2); 
+        plt.plot(degrees, cv_mses, marker='o', c='b', linestyle=file['linestyle'], label=f"{file['label']} CV MSEs", linewidth=2); 
+
+    plt.plot(degrees, np.repeat(baseline, len(degrees)), linestyle='--', label='baseline', linewidth=2)
+    plt.title("degree of polynomial vs. train and CV MSEs")
+    plt.xticks(degrees)
+    plt.xlabel("degree"); 
+    plt.ylabel("MSE"); 
+    plt.legend()
+    plt.show()
+
+model = LinearRegression()
+
+# Define properties of the 2 datasets
+file1 = {'filename':'data/c2w3_lab2_data3.csv', 'label': '3 features', 'linestyle': 'dotted'}
+file2 = {'filename':'data/c2w3_lab2_data2.csv', 'label': '2 features', 'linestyle': 'solid'}
+files = [file1, file2]
+
+#Train and plot for each dataset
+train_plot_diff_datasets(model, files, max_degree=4, baseline=250)
+
+"""
+### 2.6 Get more training examples
+
+Lastly, you can try to minimize the cross validation error by getting more examples. 
+In the cell below, you will train a 4th degree polynomial model then plot the learning curve of your model to see 
+how the errors behave when you get more examples.
+"""
+# Prepare the dataset
+x_train, y_train, x_cv, y_cv, x_test, y_test = utils.prepare_dataset('data/c2w3_lab2_data4.csv')
+print(f"the shape of the entire training set (input) is: {x_train.shape}")
+print(f"the shape of the entire training set (target) is: {y_train.shape}\n")
+print(f"the shape of the entire cross validation set (input) is: {x_cv.shape}")
+print(f"the shape of the entire cross validation set (target) is: {y_cv.shape}\n")
+
+# Instantiate the model class
+model = LinearRegression()
+
+# Define the degree of polynomial and train the model using subsets of the dataset.
+utils.train_plot_learning_curve(model, x_train, y_train, x_cv, y_cv, degree= 4, baseline=250)
+"""
+- From the results, it shows that the cross validation error starts to approach the training error as you increase the dataset size. 
+- Another insight you can get from this is that **adding more examples will not likely solve a high bias problem**. 
+  That's because the training error remains relatively flat even as the dataset increases.
+"""
 
 
 
